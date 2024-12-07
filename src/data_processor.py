4eb8475a0107278a0fce24614fb715cb96e902b9
jay0423 <jay0423@i.softbank.jp>
2024-12-08 01:33:48 +0900

    columnsとindexを追加

--- src/data_processor.py
+++ src/data_processor.py
@@ -33,7 +33,6 @@ class DataProcessor:
 
     def finalize_data(self, df, df_origin, best_individual, final_df):
         # 更新されたCOL_HOSTをdfに反映済みのため、それをdf_originにも反映する
-        # dfには対象者のみ(COL_TARGET==1)しかいないのでdf_originを更新
         for idx, row in df.iterrows():
             name = row[self.COL_NAME]
             host_count = row[self.COL_HOST]
@@ -45,6 +44,50 @@ class DataProcessor:
         df_origin.loc[df_origin[self.COL_TARGET] == 1, new_col] = best_individual
         group_df = df_origin.copy()
 
+        # final_dfの加工処理 ---------------------------------------
+        # 1. indexを"グループ1"~"グループn"にする
+        final_df.index = [f"グループ{i}" for i in range(1, len(final_df) + 1)]
+
+        # カラムリスト取得
+        cols = list(final_df.columns)
+        col_len = len(cols)
+
+        if col_len == 0:
+            # カラムがない場合は特になにもしない
+            pass
+        else:
+            # 2. 一番左の列を"リーダ"にする
+            final_df.rename(columns={cols[0]: "リーダ"}, inplace=True)
+            
+            # リネーム後のカラム再取得
+            cols = list(final_df.columns)
+
+            # 右端からpast_out_n列を "重/グループi" に、(past_out_n+1)列目を "課重なり" にする
+            # past_out_n列分の重/グループ が存在するためには col_len >= past_out_n + 2 (リーダ + 課重なり + 重/グループn)
+            # 課重なり分を含め最低 past_out_n + 1 列は右側に確保されていることが前提
+            # 仮にデータがそれに満たない場合は要件上不整合だが、ここではpast_out_nが適切に計算されることを前提とする。
+
+            # 課重なり列のインデックス取得(右からpast_out_n+1番目)
+            課重なり_idx = col_len - (past_out_n + 1)
+            if 課重なり_idx <= 0:
+                # リーダ以外に十分な列が無い場合は要件に合わないのでそのまま終了
+                pass
+            else:
+                # 一旦リーダ以降は全て"メンバ"にする
+                for i in range(1, col_len):
+                    final_df.columns.values[i] = "メンバ"
+
+                # 課重なり列をリネーム
+                final_df.columns.values[課重なり_idx] = "課重なり"
+
+                # 重/グループ列を右端からpast_out_n列分リネーム
+                # 右端から1,2,...の順で重/グループiを割り当てる
+                for i in range(1, past_out_n + 1):
+                    # 右からi番目 => インデックスは col_len - i
+                    final_df.columns.values[col_len - i] = f"重/グループ{past_out_n-i+1}"
+
+        # -------------------------------------------------------
+
         excel_file = pd.ExcelFile(self.EXCEL_NAME)
         sheet_names = excel_file.sheet_names
         sheet_dict = {sheet: excel_file.parse(sheet) for sheet in sheet_names}
@@ -54,4 +97,4 @@ class DataProcessor:
 
         with pd.ExcelWriter(self.EXCEL_NAME) as writer:
             for sheet_name, dataframe in sheet_dict.items():
-                dataframe.to_excel(writer, sheet_name=sheet_name, index=False)
+                dataframe.to_excel(writer, sheet_name=sheet_name, index=True)
